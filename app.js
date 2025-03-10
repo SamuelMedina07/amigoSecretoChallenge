24f211c94c26700edf12f544b8cdaa3c09bdb882
Samuel Medina <samuelmedina2000@gmail.com>
2025-03-10 11:11:10 -0600

    Espacios eliminados y html

--- app.js
+++ app.js
@@ -13,31 +13,23 @@ else {
 
 }
 
-
 const amigos = [];
 
 function listadoAmigos(nuevo_amigo){    
     amigos.push(nuevo_amigo);
     crearElemento("li", nuevo_amigo,"listaAmigos");
-    /* let listado = document.getElementById('listaAmigos');
-    listado.appendChild(crearElemento("li", nuevo_amigo)); */
 }
 
-function hola(){
+function sortear(){
     let ganador = Math.floor(Math.random()*amigos.length);
-    console.log(amigos[ganador]);
-    console.log(ganador);
     crearElemento("li", amigos[ganador],'resultado');
-    /* let listado = document.getElementById('resultado');
-    listado.appendChild(crearElemento("li",amigos[ganador])); */
-    
+   
 }
 
 function crearElemento(etiqueta_hija,nombre_persona,etiqueta_padre) {
     let crearNuevo = document.createElement(etiqueta_hija);
     crearNuevo.innerHTML = nombre_persona;
     agregarElemento(etiqueta_padre,crearNuevo);
-    console.log(etiqueta_padre,crearNuevo);
 }
 
  function agregarElemento(id,datos){
@@ -45,3 +37,5 @@ function crearElemento(etiqueta_hija,nombre_persona,etiqueta_padre) {
     listado.appendChild(datos);
     return listado;
 }
+
+
