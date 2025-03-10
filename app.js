ef4eded5665ee1b3cdd805ffb4f379f1097a62c6
Samuel Medina <samuelmedina2000@gmail.com>
2025-03-10 09:06:48 -0600

    ya modificada dos funciones para que sea mas facil de entender el programa

--- app.js
+++ app.js
@@ -18,22 +18,30 @@ const amigos = [];
 
 function listadoAmigos(nuevo_amigo){    
     amigos.push(nuevo_amigo);
-    let listado = document.getElementById('listaAmigos');
-    listado.appendChild(crearElemento("li", nuevo_amigo));
+    crearElemento("li", nuevo_amigo,"listaAmigos");
+    /* let listado = document.getElementById('listaAmigos');
+    listado.appendChild(crearElemento("li", nuevo_amigo)); */
 }
 
 function hola(){
     let ganador = Math.floor(Math.random()*amigos.length);
     console.log(amigos[ganador]);
     console.log(ganador);
-    let listado = document.getElementById('resultado');
-    listado.appendChild(crearElemento("li",amigos[ganador]));
+    crearElemento("li", amigos[ganador],'resultado');
+    /* let listado = document.getElementById('resultado');
+    listado.appendChild(crearElemento("li",amigos[ganador])); */
     
 }
 
-function crearElemento(etiqueta,nombre_persona) {
-    let crearNuevo = document.createElement(etiqueta);
+function crearElemento(etiqueta_hija,nombre_persona,etiqueta_padre) {
+    let crearNuevo = document.createElement(etiqueta_hija);
     crearNuevo.innerHTML = nombre_persona;
-    return crearNuevo; 
+    agregarElemento(etiqueta_padre,crearNuevo);
+    console.log(etiqueta_padre,crearNuevo);
 }
 
+ function agregarElemento(id,datos){
+    let listado = document.getElementById(id);
+    listado.appendChild(datos);
+    return listado;
+}
